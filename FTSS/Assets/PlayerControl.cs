391035d6bfbc84488150dad57744d3aba3a0e00f
mas0ny1 <masonpun3@gmail.com>
2021-10-25 12:53:20 +1100

    Added simple moving player
    Added Grey/Black as tiles into Unity
    
    Drawn simple TileMap
    
    Added Green Circle as player
    
    Changed camera to be child of player (so it follows the player)
    
    Wrote Script for player movement

---
+++ FTSS/Assets/PlayerControl.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;
+ 
+    private Rigidbody2D rb;
+
+    private float health;
+
+    void Start()
+    {
+        health = 100;
+        speed = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W)) {
+            transform.Translate (-Vector3.down * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            transform.Translate (-Vector3.right * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            transform.Translate (-Vector3.up * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            transform.Translate (-Vector3.left * Time.deltaTime * speed);
+        }
+    }
+}
