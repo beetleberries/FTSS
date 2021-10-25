fd5d6ceb9b0d988f39060e0545177f37627475a9
mas0ny1 <masonpun3@gmail.com>
2021-10-25 14:42:22 +1100

    Fixed Spaghetti from CopyPasta in Player Controls

--- FTSS/Assets/PlayerControl.cs
+++ FTSS/Assets/PlayerControl.cs
@@ -20,16 +20,16 @@ public class PlayerControl : MonoBehaviour
     void Update()
     {
         if (Input.GetKey(KeyCode.W)) {
-            transform.Translate (-Vector3.down * Time.deltaTime * speed);
+            transform.Translate (Vector3.up * Time.deltaTime * speed);
         }
         if (Input.GetKey(KeyCode.A)) {
-            transform.Translate (-Vector3.right * Time.deltaTime * speed);
+            transform.Translate (Vector3.left * Time.deltaTime * speed);
         }
         if (Input.GetKey(KeyCode.S)) {
-            transform.Translate (-Vector3.up * Time.deltaTime * speed);
+            transform.Translate (Vector3.down * Time.deltaTime * speed);
         }
         if (Input.GetKey(KeyCode.D)) {
-            transform.Translate (-Vector3.left * Time.deltaTime * speed);
+            transform.Translate (Vector3.right * Time.deltaTime * speed);
         }
     }
 }
