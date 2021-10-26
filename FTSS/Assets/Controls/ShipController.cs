0cc9b40b6ec4c5675a0b46913b11e0c3ed68036c
beetleberries <93026704+beetleberries@users.noreply.github.com>
2021-10-26 18:33:45 +1100

    beautiful rotations
    such physics

--- FTSS/Assets/Controls/ShipController.cs
+++ FTSS/Assets/Controls/ShipController.cs
@@ -7,7 +7,9 @@ public class ShipController : MonoBehaviour
 {
 
     private Rigidbody2D rb;
-    private Vector2 movement;
+    private Vector2 mouse;
+    public float speed;
+
     public PlayerInput player_input;
     public Camera character_cam;
     public Camera ship_cam;
@@ -17,30 +19,42 @@ public class ShipController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Movement Code (Speed calculations)
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * Time.deltaTime);
+
+        var dir = mouse - (Vector2) ship_cam.WorldToScreenPoint(transform.position);
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        var rot = Quaternion.AngleAxis(angle, Vector3.forward);
+        //rb.SetRotation(rot);
+        rb.AddTorque(angle / 30);
     }
+
 
     // Movement Code (Called on movement inputs)
     public void OnMovement(InputAction.CallbackContext value)
     {
-        movement = value.ReadValue<Vector2>();
+        rb.velocity = value.ReadValue<Vector2>() * speed;
     }
 
     //Called when switching from flying to boarding
     public void switch_controls(InputAction.CallbackContext value)
     {
         //If statement needed to stop unity from crashing due to a lot of function calls
-        if (value.started)
-        {
-            //Switch ActionMap to PlayerControls
-            player_input.SwitchCurrentActionMap("PlayerControls");
+        if (!value.started) return;
+        
+        //Switch ActionMap to PlayerControls
+        player_input.SwitchCurrentActionMap("PlayerControls");
             
-            //disable ship_cam, enable player_cam
-            ship_cam.gameObject.SetActive(false);
-            character_cam.gameObject.SetActive(true);
-        }
+        //disable ship_cam, enable player_cam
+        ship_cam.gameObject.SetActive(false);
+        character_cam.gameObject.SetActive(true);
+        
+    }
+
+    public void OnMouseMove(InputAction.CallbackContext value)
+    {
+        mouse = value.ReadValue<Vector2>();
+        Debug.Log(mouse);
+        //rb.MoveRotation = value.ReadValue<Vector2>();
     }
 }
