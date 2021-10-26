d9d24c0eec8772f26207b3f145de3182fd7b1813
mas0ny1 <masonpun3@gmail.com>
2021-10-26 16:33:15 +1100

    Finished camera switching POGGERS
    Hardcode controls switch
    hardcoded camera swapping

--- FTSS/Assets/Controls/PlayerController.cs
+++ FTSS/Assets/Controls/PlayerController.cs
@@ -8,22 +8,40 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody2D rb;
     private Vector2 movement;
+    public PlayerInput player_input;
+    public Camera character_cam;
+    public Camera ship_cam;
 
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+    // Movement Code (Speed calculations)
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * Time.deltaTime);
     }
 
-    // OnMovement is called on input
+    // Movement Code (Called on movement inputs)
     public void OnMovement(InputAction.CallbackContext value)
     {
         movement = value.ReadValue<Vector2>();
+    }
+
+    //Called when switching from boarding to flying
+    public void switch_controls(InputAction.CallbackContext value)
+    {
+        //If statement needed to stop unity from crashing due to a lot of function calls
+        if (value.started)
+        {
+            //Switch ActionMap to ShipControls
+            player_input.SwitchCurrentActionMap("ShipControls");
+
+            //disable player_cam, enable ship_cam
+            character_cam.gameObject.SetActive(false);
+            ship_cam.gameObject.SetActive(true);
+        }
+        
     }
 }
--- FTSS/Assets/Controls/ShipController.cs
+++ FTSS/Assets/Controls/ShipController.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ShipController : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    public PlayerInput player_input;
+    public Camera character_cam;
+    public Camera ship_cam;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    // Movement Code (Speed calculations)
+    void FixedUpdate()
     {
-        
+        rb.MovePosition(rb.position + movement * Time.deltaTime);
+    }
+
+    // Movement Code (Called on movement inputs)
+    public void OnMovement(InputAction.CallbackContext value)
+    {
+        movement = value.ReadValue<Vector2>();
+    }
+
+    //Called when switching from flying to boarding
+    public void switch_controls(InputAction.CallbackContext value)
+    {
+        //If statement needed to stop unity from crashing due to a lot of function calls
+        if (value.started)
+        {
+            //Switch ActionMap to PlayerControls
+            player_input.SwitchCurrentActionMap("PlayerControls");
+            
+            //disable ship_cam, enable player_cam
+            ship_cam.gameObject.SetActive(false);
+            character_cam.gameObject.SetActive(true);
+        }
     }
 }
